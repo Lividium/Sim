54f687d35177582631d11c8e6eb83137ef70a21d
Lividium <Lividium@gmail.com>
2015-02-17 09:54:04 -0600

    Update Queue04.h

--- Queue04.h
+++ Queue04.h
@@ -7,3 +7,23 @@ struct QueueException(char* m)
   cout << "The Queue is " << m << ".";
   cout << endl;
 }
+
+class Queue
+{
+  Queue() // this is my constructor
+  {
+    //todo fill this in
+  }
+  ~Queue() // this is my destructor
+  {
+    // todo build my destructor
+  }
+  void Enq(int v) // this member function will add a value to the queue
+  {
+    // Todo throw QueueException
+    // Todo increment the value of member "newest" using the Modulo
+  }
+}
+
+
+#endif
